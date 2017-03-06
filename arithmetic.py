3d2b70b491dd7d4b92819f7d4a0fa50b2a56db2d
ashah03 <aditshah00@gmail.com>
2017-03-06 14:16:43 -0800

    Added an add function

--- arithmetic.py
+++ arithmetic.py
@@ -1,2 +1,16 @@
 def add(a, b):
-    return a+b
+    return a + b
+def subtract(a, b):
+    return add(a, -b)
+def multiply(a, b):
+    return a*b
+def divide(a, b):
+    if b == 0:
+        return "Undefined"
+    else:
+        return multiply(a, power(b, -1))
+def power(b, x):
+    return b**x
+def root(b, n):
+    return power(b, power(n, -1))
+print(root(-1, 2))
